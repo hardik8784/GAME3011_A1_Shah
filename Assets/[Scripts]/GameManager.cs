01c6fc1b871670e7f58f71cb260d1d46b5233804
Hardik <hardik8784@gmail.com>
2022-01-30 21:24:58 -0500

    Implementing UI & Player
    Base Scene implementation

---
+++ Assets/[Scripts]/GameManager.cs
@@ -0,0 +1,28 @@
+/*
+ * Full Name: Hardik Dipakbhai Shah
+ * Student ID : 101249099
+ * Date Modified : December 30,2022
+ * File : GameManager.cs
+ * Description : This is GameManager Script
+ * Revision History : v0.1 > 
+ *              
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/[Scripts]/PlayerBehaviour.cs
@@ -0,0 +1,30 @@
+/*
+ * Full Name: Hardik Dipakbhai Shah
+ * Student ID : 101249099
+ * Date Modified : December 30,2022
+ * File : PlayerBehaviour.cs
+ * Description : This is PlayerBehaviour Script
+ * Revision History : v0.1 > Implemeted Player Movement with Player Input(Input action assets)
+ *              
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
